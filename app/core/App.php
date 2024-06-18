1a50b0e794de7523f1eaea8015c8e901801c71b4
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2024-06-18 12:38:06 +0700

    menambahkan view

--- app/core/App.php
+++ app/core/App.php
@@ -9,26 +9,28 @@ class App
     public function __construct()
     {
         $url = $this->parseURL();
-        
-        //controller
-        if( file_exists('../app/controllers/' . $url[0] . '.php') ) {
-            $this->controller = $url[0];
-            unset($url[0]);
+
+        if (isset($url[0])) {
+            //controller
+            if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+                $this->controller = $url[0];
+                unset($url[0]);
+            }
         }
 
         require_once '../app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
 
         //method 
-        if( isset($url[1])) {
-            if(method_exists($this->controller, $url[1]) ) {
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
                 $this->method = $url[1];
                 unset($url[1]);
             }
         }
 
         //params
-        if( !empty($url) ) {    
+        if (!empty($url)) {
             $this->params = array_values($url);
         }
 

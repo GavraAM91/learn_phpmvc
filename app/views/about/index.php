9139adc715bdbf2d3d3de7cd2d6f8272efad8615
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2024-06-19 18:46:11 +0700

    menambahkan beberapa asset ke dalam program saya seperti bootstrap dan gambar beserta belajar menggunakan bootstrap

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About page</h1>
-<p>Halo nama saya <?= $data['nama']; ?>, saya berumur <?= $data['umur']; ?> tahun , saya adalah seorang <?= $data['pekerjaan']; ?></p>
+<div class="container">
+    <h1 class="mt-4">About page</h1>
+    <img src="<?= BASEURL; ?>/img/bapak_gojek.jpeg" alt="Gavra" width="200" class="rounded-circle shadow">
+    <p>Halo nama saya <?= $data['nama']; ?>, saya berumur <?= $data['umur']; ?> tahun , saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>

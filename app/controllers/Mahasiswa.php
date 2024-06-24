96aa15cd2d6cd6a0857fa47e66e74341424b2f42
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2024-06-24 14:02:04 +0700

    membuat fitur cari dengan mencari berdasarkan nama, nama didapatkan dari keyword yang dikirim oleh user

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -1,7 +1,9 @@
 <?php
 
-class Mahasiswa extends Controller{
-    public function index() {
+class Mahasiswa extends Controller
+{
+    public function index()
+    {
         $data['judul'] = 'Daftar Mahasiswa';
         $data['mhs'] = $this->model('Mahasiswa_model')->getAllMahasiswa(); //mengambil seluruh data dari tabel mahasiswa 
         $this->view('template/header', $data);
@@ -9,7 +11,8 @@ class Mahasiswa extends Controller{
         $this->view('template/footer');
     }
 
-    public function detail($id) {
+    public function detail($id)
+    {
         $data['judul'] = 'Detail Mahasiswa';
         $data['mhs'] = $this->model('Mahasiswa_model')->getMahasiswaByID($id);  //mengambil data dari table mahasiswa menurut id 
         $this->view('template/header', $data); //mengirimkan data nya ke dalam header.php
@@ -17,45 +20,58 @@ class Mahasiswa extends Controller{
         $this->view('template/footer');
     }
 
-    public function tambah() {
-        
-        if($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0 ) {
+    public function tambah()
+    {
+
+        if ($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0) {
             Flasher::setFlash('berhasil', 'ditambahkan', 'success');
-            header('Location: '. BASEURL . '/Mahasiswa');
+            header('Location: ' . BASEURL . '/Mahasiswa');
             exit;
         } else {
             Flasher::setFlash('gagal', 'ditambahkan', 'danger');
-            header('Location: '. BASEURL . '/Mahasiswa');
+            header('Location: ' . BASEURL . '/Mahasiswa');
             exit;
         }
     }
 
-    public function hapus($id) {
-        
-        if($this->model('Mahasiswa_model')->hapusDataMahasiswa($id) > 0 ) {
+    public function hapus($id)
+    {
+
+        if ($this->model('Mahasiswa_model')->hapusDataMahasiswa($id) > 0) {
             Flasher::setFlash('berhasil', 'dihapus', 'success');
-            header('Location: '. BASEURL . '/Mahasiswa');
+            header('Location: ' . BASEURL . '/Mahasiswa');
             exit;
         } else {
             Flasher::setFlash('gagal', 'dihapus', 'danger');
-            header('Location: '. BASEURL . '/Mahasiswa');
+            header('Location: ' . BASEURL . '/Mahasiswa');
             exit;
         }
     }
 
-    public function getubah() {
-       echo json_encode($this->model('Mahasiswa_model')->getMahasiswaByID($_POST['id']));
+    public function getubah()
+    {
+        echo json_encode($this->model('Mahasiswa_model')->getMahasiswaByID($_POST['id']));
     }
 
-    public function ubah() {
-        if($this->model('Mahasiswa_model')->ubahDataMahasiswa($_POST) > 0 ) {
+    public function ubah()
+    {
+        if ($this->model('Mahasiswa_model')->ubahDataMahasiswa($_POST) > 0) {
             Flasher::setFlash('berhasil', 'diubah', 'success');
-            header('Location: '. BASEURL . '/Mahasiswa');
+            header('Location: ' . BASEURL . '/Mahasiswa');
             exit;
         } else {
             Flasher::setFlash('gagal', 'diubah', 'danger');
-            header('Location: '. BASEURL . '/Mahasiswa');
+            header('Location: ' . BASEURL . '/Mahasiswa');
             exit;
         }
+    }
+
+    public function cari()
+    {
+        $data['judul'] = 'Daftar Mahasiswa';
+        $data['mhs'] = $this->model('Mahasiswa_model')->cariDataMahasiswa();
+        $this->view('template/header', $data);
+        $this->view('mahasiswa/index', $data);
+        $this->view('template/footer');
     }
 }
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -66,5 +66,15 @@ class Mahasiswa_model
         return $this->db->rowCount();
     }
 
+    public function cariDataMahasiswa() {
+        
+        //menerima keyword yang dikirim
+        $keyword = $_POST['keyword'];
 
+        $query = 'SELECT * FROM ' . $this->table .' WHERE nama LIKE :keyword ';
+        $this->db->query($query);
+        $this->db->bind('keyword', "%$keyword%");
+        
+        return $this->db->resultSet();
+    }       
 }
